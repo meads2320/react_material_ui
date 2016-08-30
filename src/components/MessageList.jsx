12d929c40f2c7ba26c81c7044ce9cfa08dd7a874
HCI01\meads <meads@hcpci.com>
2016-08-30 10:30:46 -0400

    update

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -5,24 +5,32 @@ import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import firebase from 'firebase';
+import _ from 'lodash';
 
 class MessageList extends React.Component { 
     constructor(props) { 
         super(props);
          this.state = {
-            messages: [
-                'hi there',
-                'hello',
-                'how are you',
-                'good',
-                'cool'
-            ]
+            messages: []
         };
+
+
+        this.firebaseRef = firebase.initializeApp({ 
+            databaseURL: 'https://react-stack-69b29.firebaseio.com',
+            storageBucket: 'messages'
+         });
+        
+        //new Firebase('https://react-stack-69b29.firebaseio.com/messages');
+        this.firebaseRef.once('value', (dataSnapshot) => { 
+            var messages = dataSnapshot.val();
+            this.setState({ messages : messages});
+        });
     }
     render() { 
         var messageNodes = this.state.messages.map((message, index) => {
             return (
-              <Message message={message} />
+              <Message message={message.message} />
             );
         });
 
